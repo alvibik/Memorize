78dc919e6e204ba503e47420dac9606eac9fab32
Александр Биктеев <bikteev@mail.ru>
2022-12-13 09:53:10 +0600

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by albik on 13.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
