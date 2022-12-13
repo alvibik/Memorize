1cc0d111d83df3cd68f82f61b0bb332386b2d1e5
Александр Биктеев <bikteev@mail.ru>
2022-12-13 11:33:15 +0600

    Setup UI Setup basic logic

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,14 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["🚲", "🚂", "🚁", "🚜", "🚕", "🏎️", "🚑", "🚓", "🚒", "✈️", "🚀", "⛵️",
+                  "🛸", "🛶", "🚌", "🏍️", "🛺", "🚡", "🛵", "🚗", "🚚", "🚇", "🚙", "🚈"
+    ]
+    @State var emojisCount = 24
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 70))]) {
+                    ForEach(emojis[0..<emojisCount], id: \.self) { emoji in
+                        CardView(content: emoji)
+                            .aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
+            }
+            .foregroundColor(.red)
+            Spacer()
+            HStack {
+                add
+                Spacer()
+                remove
+            }
+            .font(.largeTitle)
+            .padding(.horizontal)
         }
-        .padding()
+    }
+    var add: some View {
+        Button(action: {
+            if emojisCount < emojis.count {
+                emojisCount += 1
+            }
+        }) {
+            Image(systemName: "plus.circle")
+        }
+    }
+    var remove: some View {
+        Button(action: {
+            if emojisCount > 1 {
+                emojisCount -= 1
+            }
+        }) {
+            Image(systemName: "minus.circle")
+        }
+    }
+}
+
+struct CardView: View {
+    @State var isFaceUp = true
+    let content: String
+    
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.strokeBorder(lineWidth: 3)
+                Text(content).font(.largeTitle)
+            } else {
+                shape .fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
     }
 }
 
